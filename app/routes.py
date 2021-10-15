bf69f37ffa64ededd16e7d84067c9228a19aef62
kristina <kkraft423@gmail.com>
2021-10-15 12:03:49 -0400

    added and debugged API endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,27 @@
 from flask import Blueprint
 
+hello_world_bp = Blueprint("hello_world", __name__)
+
+@hello_world_bp.route("/hello-world", methods=["GET"])
+def say_hello_world():
+    my_beautiful_response_body = "Hello, World!"
+    return my_beautiful_response_body
+
+@hello_world_bp.route("/hello/JSON", methods=["GET"])
+def say_hello_json():
+    return {
+        "name": "Kristina Kraft",
+        "message": "Yo!",
+        "hobbies": ["Chilling and whatnot"]
+    }
+
+@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
+def broken_endpoint():
+    response_body = {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
+    new_hobby = "Surfing"
+    response_body["hobbies"].append(new_hobby)
+    return response_body
